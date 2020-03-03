01169877b24e9a54bc029059c6673bd51e534d6b
arvinyung <ym@zqf.com.cn>
2020-03-03 12:23:38 +0800

    Initial commit

---
+++ scratch.py
@@ -0,0 +1,30 @@
+#   -*- coding:utf-8 -*-
+
+'''
+    @   功能：     根据身高，体重计算BMI指数
+    @   author：  无语
+    @   create：  2020-03-03
+'''
+
+print ('''根据身高，体重计算BMI指数''')
+
+#=================程序开始==================#
+#输入身高和体重
+height=float(input("请输入您的身高："))
+weight=float(input("请输入您的体重："))
+
+
+bmi=weight/(weight*height)
+
+print("您的BMI指数喂："+str(bmi))
+
+#判断身材是否合理
+if  bmi<18.5:
+    print("您的体重过轻 ～@～")
+if  bmi>=18.5 and bmi<24.9:
+    print("正常范围，请保持 （-_-）")
+if  bmi>24.9 and bmi<29.9:
+    print("您的体重过重 ～@～ ")
+if  bmi>29.9:
+    print("肥胖 ～@～")
+
